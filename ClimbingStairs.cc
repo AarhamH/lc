ddf011a3708b94cc4cf5cafc23c189cfc79a8677
AarhamH <ahaider@cayenta.com>
2025-03-15 03:19:16 -0700

    LC70 - Climbing Stairs

---
+++ ClimbingStairs.cc
@@ -0,0 +1,41 @@
+#include <vector>
+/*
+  Leetcode 70: Climbing Stairs
+
+    You are climbing a staircase. It takes n steps to reach the top.Each time you can either climb 1 or 2 steps. 
+    In how many distinct ways can you climb to the top?
+
+    Example 1:
+
+    Input: n = 2
+    Output: 2
+    Explanation: There are two ways to climb to the top.
+    1. 1 step + 1 step
+    2. 2 steps
+
+    Example 2:
+
+    Input: n = 3
+    Output: 3
+    Explanation: There are three ways to climb to the top.
+    1. 1 step + 1 step + 1 step
+    2. 1 step + 2 steps
+    3. 2 steps + 1 step
+*/
+class Solution {
+public:
+    int climbStairs(int n) {
+      std::vector<int> d(n+1);
+       if (n <=2) return n;
+
+       d[0] = 0; 
+       d[1] = 1;
+       d[2] = 2; 
+       for(int i=3; i<=n; i++) {
+        d[i] = d[i-1] + d[i-2];
+       } 
+
+       return d[n];
+    }
+};
+
