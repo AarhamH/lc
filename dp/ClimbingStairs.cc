5f1bbc151ae2011ac728cda7b2ce835684c5e764
AarhamH <ahaider@cayenta.com>
2025-03-16 21:46:08 -0700

    Added difficulty

--- dp/ClimbingStairs.cc
+++ dp/ClimbingStairs.cc
@@ -1,6 +1,6 @@
 #include <vector>
 /*
-  Leetcode 70: Climbing Stairs
+  Leetcode 70: Climbing Stairs (EASY)
 
     You are climbing a staircase. It takes n steps to reach the top.Each time you can either climb 1 or 2 steps. 
     In how many distinct ways can you climb to the top?
