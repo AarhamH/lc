5c824911cc922d38cfd00a8db80bb181c4df7179
AarhamH <aarham.haider@gmail.com>
2025-04-17 23:13:42 -0700

    LC127 - Word Ladder

---
+++ graphs/WordLadder.cc
@@ -0,0 +1,71 @@
+#include <unordered_set>
+#include<vector>
+#include<string>
+#include<deque>
+
+/*
+  Leetcode 127: Word Ladder (HARD)
+  A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+      Every adjacent pair of words differs by a single letter.
+      Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+      sk == endWord
+
+  Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+   
+
+  Example 1:
+
+  Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+  Output: 5
+  Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+
+  Example 2:
+
+  Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+  Output: 0
+  Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+*/
+
+using namespace std;
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> s(wordList.begin(), wordList.end());
+        int path = 0;
+        deque<string> q;
+        q.push_back(beginWord);
+
+        while(!q.empty()) {
+            path++;
+            int len = q.size();
+            for(int i=0; i<len; i++) {
+                string node = q.front();
+                q.pop_front();
+                if(node == endWord) {
+                    return path;
+                }
+                for(int j=0; j<node.length(); j++) {
+                    char og = node[j];
+                    char c = 'a';
+                    while(c <= 'z') {
+                        if(c == og) {
+                            c++;
+                            continue;
+                        }
+                        node[j] = c;
+                        if(s.find(node) != s.end()) {
+                            q.push_back(node);
+                            s.erase(node);
+                        }
+                        c++;
+                    }
+                    node[j] = og;
+                }
+            }
+        }
+
+        return 0;
+    }
+};
