11174437829fefd3caf95eca0e2e049562e65144
AarhamH <aarham.haider@gmail.com>
2025-04-19 15:30:34 -0700

    LC63 - Unqiue Paths

---
+++ dp/UniquePaths.cc
@@ -0,0 +1,49 @@
+#include<vector>
+
+/*
+  Leetcode 62: Unique Paths (MEDIUM)
+
+  There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+  Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+  The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+  Example 1:
+
+  Input: m = 3, n = 7
+  Output: 28
+
+  Example 2:
+
+  Input: m = 3, n = 2
+  Output: 3
+  Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+  1. Right -> Down -> Down
+  2. Down -> Down -> Right
+  3. Down -> Right -> Down
+*/
+
+using namespace std;
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+       vector<vector<int>>d(m, vector<int>(n));
+       
+       for(int i=0; i<m; i++) {
+        d[i][0] = 1;
+       }
+
+       for(int j=0; j<n; j++) {
+        d[0][j] = 1;
+       }
+
+       for(int i=1; i<m; i++) {
+        for(int j=1; j<n; j++) {
+          d[i][j] = d[i-1][j] + d[i][j-1];
+        }
+       }
+
+       return d[m-1][n-1]; 
+    }
+};
